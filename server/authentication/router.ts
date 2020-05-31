4d875d5925ba456784d6af494e9728bd3c894b71
may kremer <maykremer@gmail.com>
2020-05-31 20:13:46 +0300

    delete old auth

--- server/authentication/router.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { AuthenticationHandler } from './handler';
-
-const AuthenticationRouter = Router();
-
-AuthenticationRouter.get('/login', AuthenticationHandler.authenticate(), (req, res) => res.status(200).json(req.user));
-AuthenticationRouter.post('/callback', AuthenticationHandler.authenticate(), (req, res) => res.redirect('/'));
-
-export { AuthenticationRouter };
--- server/server.ts
+++ server/server.ts
@@ -3,16 +3,8 @@ import * as bodyParser from "body-parser";
 import * as mongoose from "mongoose";
 import PersonRouter from "./personRouter";
 import config from "./config";
-import { AuthenticationMiddleware } from "./authentication/middleware";
-import { AuthenticationHandler } from "./authentication/handler";
-import { AuthenticationRouter } from "./authentication/router";
 import cors from 'cors';
-const session = require("express-session");
-const cookieParser = require("cookie-parser");
-const passport = require("passport");
 const path = require('path');
-
-// const shraga = require("./passport.js");
 
 const app = express();
 const SERVER_PORT: number = config.serverPort;
@@ -29,22 +21,6 @@ export class Server {
     this.app.use(cors());
     this.app.use(this.setHeaders);
     this.app.use(bodyParser.json());
-    this.app.use(cookieParser());
-    this.app.use(passport.initialize());
-    this.app.use(passport.session({
-      secret: config.auth.secret,
-      resave: false,
-      saveUninitialized: true
-    }));
-    // console.log("asd");
-    this.initializeAuthenticator();
-    // this.app.use((req, res, next) => {
-    //   if (!req.user)
-    //   res.redirect("/auth")
-    //   else
-    //   next();
-    // });
-    this.app.get('/user', AuthenticationMiddleware.requireAuth, (req: express.Request, res: express.Response, next: express.NextFunction) => res.send(req.user));
     this.app.use(PersonRouter);
     this.app.listen(SERVER_PORT, () => {
       console.log(`server is listening on port ${SERVER_PORT}`);
@@ -74,8 +50,4 @@ export class Server {
     return new Server();
   }
   
-  private initializeAuthenticator() {
-    AuthenticationHandler.initialize(this.app);
-    // this.app.use('/auth/', AuthenticationRouter);
-}
 }
